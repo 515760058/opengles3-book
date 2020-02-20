3c437726108890a53bfc17e969be68755c6f18d9
515760058 <515760058@qq.com>
2020-02-21 01:00:28 +0800

    读代码 加注释

--- Chapter_11/MRTs/MRTs.c
+++ Chapter_11/MRTs/MRTs.c
@@ -36,7 +36,7 @@
 //    per fragment using MRTs.
 //    Then, we will copy the four color buffers into four screen quadrants
 //    using framebuffer blits.
-//
+//  11.7节  多重渲染目标
 #include <stdlib.h>
 #include "esUtil.h"
 
@@ -73,39 +73,39 @@ int InitFBO ( ESContext *esContext )
       GL_COLOR_ATTACHMENT3 
    };
 
-   glGetIntegerv ( GL_FRAMEBUFFER_BINDING, &defaultFramebuffer );
+   glGetIntegerv ( GL_FRAMEBUFFER_BINDING, &defaultFramebuffer );//defaultFramebuffer的值是0
 
-   // Setup fbo
+   // Setup fbo 生成并绑定fbo
    glGenFramebuffers ( 1, &userData->fbo );
    glBindFramebuffer ( GL_FRAMEBUFFER, userData->fbo );
 
-   // Setup four output buffers and attach to fbo
+   // Setup four output buffers and attach to fbo 生成四个纹理，作为fbo的attachment
    userData->textureHeight = userData->textureWidth = 400;
    glGenTextures ( 4, &userData->colorTexId[0] );
    for (i = 0; i < 4; ++i)
    {
       glBindTexture ( GL_TEXTURE_2D, userData->colorTexId[i] );
-
+      // 为每个纹理申请GPU内存
       glTexImage2D ( GL_TEXTURE_2D, 0, GL_RGBA, 
                      userData->textureWidth, userData->textureHeight, 
                      0, GL_RGBA, GL_UNSIGNED_BYTE, NULL );
 
-      // Set the filtering mode
+      // Set the filtering mode 设置过滤模式
       glTexParameteri ( GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST );
       glTexParameteri ( GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST );
-
+      // 纹理附着到fbo作为attachment   注意参数attachments[i]
       glFramebufferTexture2D ( GL_DRAW_FRAMEBUFFER, attachments[i], 
                                GL_TEXTURE_2D, userData->colorTexId[i], 0 );
    }
-
+   //指定片段颜色要写入的缓冲区（颜色attachment）
    glDrawBuffers ( 4, attachments );
 
    if ( GL_FRAMEBUFFER_COMPLETE != glCheckFramebufferStatus ( GL_FRAMEBUFFER ) )
    {
       return FALSE;
    }
-
-   // Restore the original framebuffer
+   //printf("=== %d\n", defaultFramebuffer);
+   // Restore the original framebuffer 重新绑定为原来的fbo
    glBindFramebuffer ( GL_FRAMEBUFFER, defaultFramebuffer );
 
    return TRUE;
@@ -128,7 +128,7 @@ int Init ( ESContext *esContext )
    char fShaderStr[] =
        "#version 300 es                                     \n"
        "precision mediump float;                            \n"
-       "layout(location = 0) out vec4 fragData0;            \n"
+       "layout(location = 0) out vec4 fragData0;            \n"//四个输出颜色
        "layout(location = 1) out vec4 fragData1;            \n"
        "layout(location = 2) out vec4 fragData2;            \n"
        "layout(location = 3) out vec4 fragData3;            \n"
@@ -178,9 +178,10 @@ void DrawGeometry ( ESContext *esContext )
    // Use the program object
    glUseProgram ( userData->programObject );
 
-   // Load the vertex position
+   // Load the vertex position  指明顶点位置的数据格式，和数据地址（其实没有用到顶点数据）
    glVertexAttribPointer ( 0, 3, GL_FLOAT,
                            GL_FALSE, 3 * sizeof ( GLfloat ), vVertices );
+   //使能顶点数组
    glEnableVertexAttribArray ( 0 );
 
    // Draw a quad
@@ -195,30 +196,30 @@ void BlitTextures ( ESContext *esContext )
    UserData *userData = esContext->userData;
 
    // set the fbo for reading
-   glBindFramebuffer ( GL_READ_FRAMEBUFFER, userData->fbo );
+   glBindFramebuffer ( GL_READ_FRAMEBUFFER, userData->fbo );//设置源fbo，从中读取。
  
    // Copy the output red buffer to lower left quadrant
-   glReadBuffer ( GL_COLOR_ATTACHMENT0 );
-   glBlitFramebuffer ( 0, 0, userData->textureWidth, userData->textureHeight,
-                       0, 0, esContext->width/2, esContext->height/2, 
-                       GL_COLOR_BUFFER_BIT, GL_LINEAR );
+   glReadBuffer ( GL_COLOR_ATTACHMENT0 );//选择读取的fbo的attachment
+   glBlitFramebuffer ( 0, 0, userData->textureWidth, userData->textureHeight,//源fbo的color attachment的宽高：400*400
+                       0, 0, esContext->width/2, esContext->height/2, //目的fbo中，目的区域的宽高，左下角，红色
+                       GL_COLOR_BUFFER_BIT, GL_LINEAR );//指定要读取的缓冲区，GL_COLOR_BUFFER_BIT表示颜色缓冲区, GL_DEPTH_BUFFER_BIT表示深度缓冲区
 
    // Copy the output green buffer to lower right quadrant
    glReadBuffer ( GL_COLOR_ATTACHMENT1 );
    glBlitFramebuffer ( 0, 0, userData->textureWidth, userData->textureHeight,
-                       esContext->width/2, 0, esContext->width, esContext->height/2, 
+                       esContext->width/2, 0, esContext->width, esContext->height/2,// 右下角，绿色
                        GL_COLOR_BUFFER_BIT, GL_LINEAR );
 
    // Copy the output blue buffer to upper left quadrant
    glReadBuffer ( GL_COLOR_ATTACHMENT2 );
    glBlitFramebuffer ( 0, 0, userData->textureWidth, userData->textureHeight,
-                       0, esContext->height/2, esContext->width/2, esContext->height, 
+                       0, esContext->height/2, esContext->width/2, esContext->height, //左上角 蓝色
                        GL_COLOR_BUFFER_BIT, GL_LINEAR );
 
    // Copy the output gray buffer to upper right quadrant
    glReadBuffer ( GL_COLOR_ATTACHMENT3 );
    glBlitFramebuffer ( 0, 0, userData->textureWidth, userData->textureHeight,
-                       esContext->width/2, esContext->height/2, esContext->width, esContext->height, 
+                       esContext->width/2, esContext->height/2, esContext->width, esContext->height,//右上角 灰色 
                        GL_COLOR_BUFFER_BIT, GL_LINEAR );
 }
 
@@ -237,19 +238,20 @@ void Draw ( ESContext *esContext )
       GL_COLOR_ATTACHMENT3 
    };
    
-   glGetIntegerv ( GL_FRAMEBUFFER_BINDING, &defaultFramebuffer );
+   glGetIntegerv ( GL_FRAMEBUFFER_BINDING, &defaultFramebuffer );//原来的fbo，是0
 
    // FIRST: use MRTs to output four colors to four buffers
-   glBindFramebuffer ( GL_FRAMEBUFFER, userData->fbo );
+   glBindFramebuffer ( GL_FRAMEBUFFER, userData->fbo );//输出到这个fbo（的4个纹理attachment）
    glClear ( GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT );
-   glDrawBuffers ( 4, attachments );
+   glDrawBuffers ( 4, attachments );//为渲染指定要写入的缓冲区（例子中是颜色attachment）
+   //
    DrawGeometry ( esContext );
 
-   // SECOND: copy the four output buffers into four window quadrants
-   // with framebuffer blits
+   // SECOND: copy the four output buffers into four window quadrants with framebuffer blits
+   // 第二步：把fbo中的四个attachment中的数据，拷贝出来
 
    // Restore the default framebuffer
-   glBindFramebuffer ( GL_DRAW_FRAMEBUFFER, defaultFramebuffer );
+   glBindFramebuffer ( GL_DRAW_FRAMEBUFFER, defaultFramebuffer );//绑定（要写入的）目的fbo，下一个函数中还绑定了源fbo。
    BlitTextures ( esContext );
 }
 
@@ -273,7 +275,7 @@ void ShutDown ( ESContext *esContext )
 int esMain ( ESContext *esContext )
 {
    esContext->userData = malloc ( sizeof ( UserData ) );
-
+   //注意窗口大小400*400，initFBO中每个纹理的宽高也都是400（使用对应的过滤模式）
    esCreateWindow ( esContext, "Multiple Render Targets", 400, 400, ES_WINDOW_RGB | ES_WINDOW_ALPHA );
 
    if ( !Init ( esContext ) )
